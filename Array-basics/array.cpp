71e98e39af76989b79ed04437914a55ad504f97d
Ishtiaq khan <ishtiyaqkhan1198@gmail.com>
2026-08-18 12:32:07 +0500

    feat(l-b-search):Add linear and binary search algorithm with some array operations

--- Array-basics/array.cpp
+++ Array-basics/array.cpp
@@ -2,8 +2,47 @@
 using namespace std;
 
 
-int main()
-{  /*
+/*
+#include <iostream>
+
+using namespace std;
+
+
+struct Array{
+    int *A;
+    int length;
+    int size;
+};
+
+void display(struct Array arr){
+  int i;
+  cout<<"\n Elements are \n";
+  for(i=0;i<arr.length;i++)
+   cout<<"index : "<<i<<" element : "<<arr.A[i]<<endl;
+}
+int main() {
+    struct Array arr;
+    int n,i;
+    cout<<"Enter array size : ";
+    cin>>arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+    cout<<"/n Enter number of numbers : ";
+    cin>>n;
+    cout<<"Enter all elements : ";
+    for(i=0;i<n;i++)
+     cin>>arr.A[i];
+     arr.length = n;
+arr.length = n;
+    arr.length = n;
+     display(arr);
+    return 0 arr.length = n;
+     display(arr);
+    return 0;
+}
+
+*/
+/*
      //create an array of pointer of  size 3
     //then store the 3 array created at heap 
     int *arr[3];
@@ -12,9 +51,7 @@ int main()
     arr[2]=new int[4]{9,99,22,44};
 
     cout<<"Arr value at heap  is : "<<arr[1][3];
-    */
-   
-   //                                                        
+       //                                                        
    //inside the heap 3rdmethod
    
     int **A;
@@ -38,6 +75,120 @@ int main()
     }
 
 
+void display(struct Array arr){
+  int i;
+  cout<<"\n Elements are \n";
+  for(i=0;i<arr.length;i++)
+   cout<<"index : "<<i<<" element : "<<arr.A[i]<<endl;
+}
+void Append(struct Array *arr,int x){
+    if(arr->length<arr->size){
+        arr->A[arr->length++]=x;
+    }
+}
 
-return 0;
+void Insert(struct Array *arr,int index,int x){
+    int i;
+    if(index>=0 && index<=arr->length){
+        for(i=arr->length;i>index;i--)
+        arr->A[i] = arr->A[i-1];
+        arr->A[index] = x;
+        arr->length++;
+    }
+}
+int deletee(struct Array *arr,int index){
+    int x=0,i;
+    if(index>=0 && index<arr->length){
+        x=arr->A[index];
+        for(i=index;i<arr->length-1;i++)
+        arr->A[i]=arr->A[i+1];
+        arr->length--;
+        return x;
+    }
+    return 0;
+}
+int main() {
+    struct Array arr={{1,2,3,4,5},5,20};
+     cout<<"deleted element is : "<<deletee(&arr,2);
+     display(arr);
+    return 0;
+}
+    */
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+    /*
+    Linear Search 
+    struct Array{
+    int A[20];
+    int length;
+    int size;
+};
+
+void display(struct Array arr){
+  int i;
+  cout<<"\n Elements are \n";
+  for(i=0;i<arr.length;i++)
+   cout<<"index : "<<i<<" element : "<<arr.A[i]<<endl;
+}
+
+int linearSearch(struct Array arr,int x){
+    for(int i=0;i<arr.length-1;i++)
+     if(arr.A[i]==x) return arr.A[i];
+     return -1;
+}
+
+
+int main() {
+    struct Array arr={{1,2,3,4,5},5,20};
+     //check element 4 in a given array
+     cout<<"Checking 4 in the array : "<<linearSearch(arr,4);
+     display(arr);
+    return 0;
+}
+    
+    */
+struct Array{
+    int A[20];
+    int length;
+    int size;
+};
+
+void display(struct Array arr){
+  int i;
+  cout<<"\n Elements are \n";
+  for(i=0;i<arr.length;i++)
+   cout<<"index : "<<i<<" element : "<<arr.A[i]<<endl;
+}
+void swap(int *x,int *y){
+    int temp = *x;
+    *x=*y;
+    *y=temp;
+}
+//transposition
+int l_search_improved1(struct Array *arr,int x){
+    for(int i=0;i<arr->length-1;i++)
+     if(arr->A[i]==x){
+        swap(&arr->A[i],&arr->A[i-1]); 
+        return arr->A[i];
+    }
+     return -1;
+}
+//move to front if found 
+int l_search_improved2(struct Array *arr,int x){
+    for(int i=0;i<arr->length-1;i++)
+     if(arr->A[i]==x){
+        swap(&arr->A[i],&arr->A[0]); 
+        return arr->A[i];
+    }
+     return -1;
+}
+
+
+int main() {
+    struct Array arr={{1,2,3,4,5},5,20};
+     //check element 4 in a given array
+     cout<<"Checking 4 in the array : "<<l_search_improved1(&arr,4);
+      cout<<"Checking 4 in the array : "<<l_search_improved2(&arr,4);
+     display(arr);
+    return 0;
 }
