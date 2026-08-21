484fd411f905a6c673b8916b0cb9088d3f55e913
Ishtiaq khan <ishtiyaqkhan1198@gmail.com>
2026-08-21 21:50:05 +0500

    feat(arr-basics):add medium level  array practice questions

--- Array-basics/array-operation.cpp
+++ Array-basics/array-operation.cpp
@@ -57,7 +57,7 @@ int  deletee(struct Array *arr,int index){
     int i,x=0;
     if(index>=0 && index<arr->length){
         x=arr->A[index];
-        for(i=0;i<arr->length-1;i++)
+        for(i=index;i<arr->length-1;i++)
         arr->A[i]=arr->A[i+1];
         arr->length--;
         return x;
@@ -65,9 +65,111 @@ int  deletee(struct Array *arr,int index){
     return 0;
 
 }
+
+
+int BinarySearch(struct Array arr,int key){
+    int l,mid,h;
+    l=0;
+    h=arr.length-1;
+    while(l<=h){
+        mid=(l+h)/2;
+        if(key==arr.A[mid])
+           return mid;
+        else if(key<arr.A[mid])
+         h=mid-1;
+         else
+         l=mid+1;   
+    }
+    return -1;
+
+}
+ 
+
+int RBinSearch(int a[],int l ,int h ,int key){
+    int mid;
+    if(l<=h){
+        mid=(l+h)/2;
+        if(key==a[mid])
+        return mid;
+        else if(key<a[mid])
+        return RBinSearch(a,l,mid-1,key);
+        else
+        return RBinSearch(a,mid+1,h,key);
+
+    }
+}
+
+
+int Get(struct Array arr,int index){
+ if(index>=0 && index<arr.length)
+    return arr.A[index];
+}
+
+
+void Set(struct Array arr,int index ,int x){
+ if(index>=0 && index<arr.length)
+    arr.A[index] = x;
+}
+
+int  Max(struct Array arr){
+    int MAX = arr.A[0],i;
+    for(i=1;i<arr.length;i++)
+    if(arr.A[i]>MAX)  MAX = arr.A[i];
+    return MAX;
+}
+
+int  Min(struct Array arr){
+    int MIN = arr.A[0],i;
+    for(i=1;i<arr.length;i++)
+    if(arr.A[i]<MIN)  MIN = arr.A[i];
+    return MIN;
+}
+
+int Sum(struct Array arr){
+    int sum = 0,i;
+    for(i=0;i<arr.length;i++){
+           sum +=arr.A[i];
+    }
+    return sum;
+}
+int Avg(struct Array arr){
+    int total = 0,i;
+    for(i=0;i<arr.length;i++){
+           total +=arr.A[i];
+    }
+    return total/arr.length;
+}
+
+
+void Reverse(struct Array *arr){
+    int i,j;
+    int B[arr->length];
+    for(i=arr->length-1,j=0;i>=0;j++,i--){
+       B[j] = arr->A[i];
+    }
+    for(i=0;i<=arr->length-1;i++)
+     arr->A[i]=B[i];
+}
+
+void Reverse1(struct Array *arr){
+    int temp;
+    for(int i=0,j=arr->length-1;i<j;i++,j--){
+      temp = arr->A[i];
+      arr->A[i] = arr->A[j];
+      arr->A[j] = temp;
+    }
+}
+
+void shiftleft(struct Array *arr){
+     int i;
+     arr->A[0]=0;
+     for(i=0;i<arr->length;i++)
+      arr->A[i]=arr->A[i+1];
+}
+
 int main(){
-   struct Array arr = {{2,3,4,5,6},20,6};
-   deletee(&arr,4);
+   struct Array arr ={{1,2,3,4,5},20,5};
+   Reverse1(&arr);
    Display(arr);
     return 0;
 }
